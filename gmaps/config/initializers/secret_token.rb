98914cab029b62b3738ce7c021494777c683bf32
Manuel Vasquez de Velasco Garay <lito0512@hotmail.com>
2013-06-01 11:04:05 -0500

    Inv. Gmaps

---
+++ gmaps/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gmaps::Application.config.secret_token = 'c3a5faa5c359eca642fa49ebd30c9cb52a9e8540921ce7dcb0c76a01ff54457865f80a87a7b172fb43bf0237be72cd4a5548a68f50b17245d6f146b3aa35a8c2'
