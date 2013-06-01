5a7938b89a8ff67a2a7ba89609eb9800a45258e4
marlontaro <marloncordova@outlook.com>
2013-06-01 09:57:41 -0500

    actualizacion

--- AppConcert/app/models/concert.rb
+++ AppConcert/app/models/concert.rb
@@ -2,5 +2,5 @@ class Concert < ActiveRecord::Base
   belongs_to :user
   belongs_to :place
   belongs_to :group
-  attr_accessible :date, :end_time, :start_time, :state
+  attr_accessible :date, :end_time, :start_time, :state,:user_id, :place_id, :group_id
 end
--- AppConcert/app/models/group.rb
+++ AppConcert/app/models/group.rb
@@ -1,4 +1,4 @@
 class Group < ActiveRecord::Base
   belongs_to :style
-  attr_accessible :image, :name
+  attr_accessible :image, :name, :style_id
 end
