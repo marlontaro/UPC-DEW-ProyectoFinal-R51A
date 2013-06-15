6eb394acabb90892a490b522383289016fbd56e7
marlontaro <marloncordova@outlook.com>
2013-06-15 09:16:34 -0500

    actualizacion comentario

--- AppConcert/app/controllers/search_controller.rb
+++ AppConcert/app/controllers/search_controller.rb
@@ -1,31 +1,31 @@
 class SearchController < ApplicationController
   def new
    
-  		# @concerts = Concert.find(:all, :conditions => ['name like ?',"%#{params[:search]}%"])
- if current_user
-      if(params[:fecha])
-         
-         event = params[:fecha]
-          fecha = Date.new event["Fecha(1i)"].to_i, event["Fecha(2i)"].to_i, event["Fecha(3i)"].to_i
+      		# @concerts = Concert.find(:all, :conditions => ['name like ?',"%#{params[:search]}%"])
+     if current_user
+          if(params[:fecha])
+             
+             event = params[:fecha]
+              fecha = Date.new event["Fecha(1i)"].to_i, event["Fecha(2i)"].to_i, event["Fecha(3i)"].to_i
 
-    		@concerts = Concert.joins(:group => :style, :place => :district).where(
-  			"groups.name like ? and styles.name like ? and districts.name like ? and date(concerts.date) = ? ",
-  			"%#{params[:search]}%","%#{params[:estilo]}%","%#{params[:distrito]}%", fecha)
+        		@concerts = Concert.joins(:group => :style, :place => :district).where(
+      			"groups.name like ? and styles.name like ? and districts.name like ? and date(concerts.date) = ? ",
+      			"%#{params[:search]}%","%#{params[:estilo]}%","%#{params[:distrito]}%", fecha)
 
-        @lugar=  @concerts.select("DISTINCT places.id ")
-  				 
-      else
-        @concerts = Concert.joins(:group => :style, :place => :district).where("date(concerts.date) = ?", DateTime.now.to_date )
+            @lugar=  @concerts.select("DISTINCT places.id ")
+      				 
+          else
+            @concerts = Concert.joins(:group => :style, :place => :district).where("date(concerts.date) = ?", DateTime.now.to_date )
 
-        @lugar=  @concerts.select("DISTINCT places.id ")
-      
-      end 	
+            @lugar=  @concerts.select("DISTINCT places.id ")
+          
+          end 	
 
-        else
-    
-      redirect_back_or_to root_url, :notice => "Inicie sesion!"   
-    
-    end 
+            else
+        
+          redirect_back_or_to root_url, :notice => "Inicie sesion!"   
+        
+        end 
      
   end
 
@@ -42,11 +42,20 @@ class SearchController < ApplicationController
   end
 
   def show
+
+if current_user
     @concerts = Concert.find(params[:id])
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @concerts }
     end
+  else
+        
+          redirect_back_or_to root_url, :notice => "Inicie sesion!"   
+        
+        end 
+    
+
   end
 
 end
